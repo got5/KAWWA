574cb9fba9a7e5779a49d3013d3b228a6f9a4548
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2013-02-05 15:03:00 +0100

    Issue #36 ProductQuantity use addClass()

--- Portal/kawwa_components/08_ecommerce/06_productQuantity/snippet.js
+++ Portal/kawwa_components/08_ecommerce/06_productQuantity/snippet.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
 	var incTest = incrementTest();
 
 	if (jQuery.fn.uppydowner) {
-		if(!incTest) {
-			jQuery('input[type=number]').attr('class', 'uppydowner');
+		if(incTest) {
+			jQuery('input[type=number]').addClass('uppydowner');
 			jQuery('input.uppydowner').uppydowner();
 		} else {
 			jQuery('input[type=number]').css('width', '3em');
