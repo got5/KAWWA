c1d659289444da081afecc37aeee8d6340ec24b8
Emmanuel <demey.emmanuel@gmail.com>
2013-04-15 15:51:29 +0200

    First Commit for the Angular Implementation

---
+++ kawwa2-angularjs/kawwa-angular.js
@@ -0,0 +1,45 @@
+var isotopeApp = angular.module('kawwa', []);
+
+
+function putObject(path, object, value) {
+    var modelPath = path.split(".");
+
+    function fill(object, elements, depth, value) {
+        var hasNext = ((depth + 1) < elements.length);
+        if(depth < elements.length && hasNext) {
+            if(!object.hasOwnProperty(modelPath[depth])) {
+                object[modelPath[depth]] = {};
+            }
+            fill(object[modelPath[depth]], elements, ++depth, value);
+        } else {
+            object[modelPath[depth]] = value;
+        }
+    }
+    fill(object, modelPath, 0, value);
+}
+
+angular.module('kawwa').directive('kawwaRaty', function () {
+    return {
+        restrict: 'A',
+
+        link:function (scope, element, attrs, controller) {
+            var json = jQuery.extend({
+                callback: function(value){
+                    putObject(jQuery(element).attr('ng-model'), scope, value);
+                    if(!scope.$$phase) scope.$apply();
+                }
+            }, scope.$eval(attrs.kawwaRatingJson))
+            jQuery(element).children("input").rating(json);
+
+            scope.$watch(attrs.ngModel, function(value){
+                jQuery(element).children("input").rating("select", ""+value);
+            },true)
+
+        }
+    };
+});
+
+
+
+
+
