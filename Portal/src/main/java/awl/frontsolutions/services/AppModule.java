4139a2094b471ebd39c2a4668390fd5167c3ea77
Emmanuel <demey.emmanuel@gmail.com>
2013-08-19 21:05:37 +0200

    Some work on the Gradle build file

--- Portal/src/main/java/awl/frontsolutions/services/AppModule.java
+++ Portal/src/main/java/awl/frontsolutions/services/AppModule.java
@@ -8,6 +8,7 @@ import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.MetaDataConstants;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
@@ -31,6 +32,7 @@ import org.got5.tapestry5.jquery.services.javascript.FormSupportStack;
 import org.slf4j.Logger;
 
 import awl.frontsolutions.pages.GAnalyticsScriptsInjector;
+import awl.frontsolutions.services.atos.AtosModule;
 import awl.frontsolutions.services.impl.AtosServiceDefaultImpl;
 import awl.frontsolutions.services.impl.AuthentificationDefaultImpl;
 import awl.frontsolutions.services.impl.ComponentUtilsImpl;
@@ -46,119 +48,130 @@ import awl.frontsolutions.services.stack.Theme1Stack;
 import awl.frontsolutions.services.stack.ThemeStack;
 import awl.frontsolutions.services.stack.ThemeSwitcherStack;
 
-//@SubModule(AtosModule.class)
+@SubModule(AtosModule.class)
 public class AppModule {
-	
+
 	@Startup
-	public static void parseComponens(FileSystemIndexer indexer, TopComponent topComponent){
+	public static void parseComponens(FileSystemIndexer indexer,
+			TopComponent topComponent) {
 		topComponent.setTopComponent();
 	}
-	
-	
+
 	@Contribute(SymbolProvider.class)
 	@ApplicationDefaults
-	public static void configure(MappedConfiguration<String, String> configuration) {
+	public static void configure(
+			MappedConfiguration<String, String> configuration) {
 
 		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en, fr");
 
 		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
-		
+
 		configuration.add(SymbolConstants.COMPRESS_WHITESPACE, "false");
 
-		configuration.add(SymbolConstants.APPLICATION_VERSION, "0.0.2-SNAPSHOT");
-		
-		configuration.add(JQuerySymbolConstants.JQUERY_UI_DEFAULT_THEME,"context:css/k-structure.css");
-		
+		configuration
+				.add(SymbolConstants.APPLICATION_VERSION, "0.0.2-SNAPSHOT");
+
+		configuration.add(JQuerySymbolConstants.JQUERY_UI_DEFAULT_THEME,
+				"context:css/k-structure.css");
+
 		configuration.add(KawwaConstants.KAWWA_INCLUDE_STACK, "false");
-		
+
 		configuration.add("IncludeStack", "false");
-		
+
 		configuration.add("enableAnalytics", "false");
-		
+
 		configuration.add("enableDocumentationBlock", "false");
 	}
 
-	public void contributeMetaDataLocator(MappedConfiguration<String,String> configuration)
-	{
-	    configuration.add(MetaDataConstants.SECURE_PAGE, "true");
+	public void contributeMetaDataLocator(
+			MappedConfiguration<String, String> configuration) {
+		configuration.add(MetaDataConstants.SECURE_PAGE, "true");
 	}
-	
+
 	@Contribute(JavaScriptStackSource.class)
 	public static void addKawwa2Themes(
 			MappedConfiguration<String, JavaScriptStack> configuration) {
 		configuration.addInstance("themestack", ThemeSwitcherStack.class);
 		configuration.addInstance(ThemeStack.DEFAULT_THEME, Theme0Stack.class);
-		configuration.addInstance(ThemeStack.PREFIX+"k-theme1", Theme1Stack.class);
-		//configuration.addInstance(ThemeStack.PREFIX+"k-theme2", Theme2Stack.class);
-		
-		
-		configuration.overrideInstance(JQuerySymbolConstants.PROTOTYPE_STACK, OverridePrototypeStack.class);
-		configuration.overrideInstance(FormSupportStack.STACK_ID, OverrideFormSupportStack.class);
-	}
+		configuration.addInstance(ThemeStack.PREFIX + "k-theme1",
+				Theme1Stack.class);
+		// configuration.addInstance(ThemeStack.PREFIX+"k-theme2",
+		// Theme2Stack.class);
 
+		configuration.overrideInstance(JQuerySymbolConstants.PROTOTYPE_STACK,
+				OverridePrototypeStack.class);
+		configuration.overrideInstance(FormSupportStack.STACK_ID,
+				OverrideFormSupportStack.class);
+	}
 
 	public static void bind(ServiceBinder binder) {
 
 		binder.bind(FileSystemIndexer.class, FileSystemIndexerImpl.class);
 		binder.bind(ComponentZipFiller.class, ComponentZipFillerImpl.class);
-		binder.bind(ComponentUtils.class, ComponentUtilsImpl.class);		
+		binder.bind(ComponentUtils.class, ComponentUtilsImpl.class);
 		binder.bind(DASSecurityManager.class, DASSecurityManagerImpl.class);
 		binder.bind(TopComponent.class, TopComponentImpl.class);
 		binder.bind(AtosService.class, AtosServiceDefaultImpl.class);
 		binder.bind(Authentification.class, AuthentificationDefaultImpl.class);
 		binder.bind(MailService.class, DefaultMailServiceImpl.class);
-	}	
-	
-	
-	@Contribute(MarkupRenderer.class) 
-    public static void addMetaRenderFilter( 
-            OrderedConfiguration<MarkupRendererFilter> configuration, 
-            final Environment environment, @Symbol("enableAnalytics") final boolean enableAnalytics
-    ) { 
-        MarkupRendererFilter rendererFilter = new MarkupRendererFilter() { 
-            public void renderMarkup(MarkupWriter writer,MarkupRenderer renderer) { 
-               
-                renderer.renderMarkup(writer); 
-                Element el = writer.getDocument().getRootElement().find("head");
-                if(el!=null){
-                	el.elementAt(0, "meta", "content", "IE=edge", "http-equiv" , "X-UA-Compatible");
-                	el.elementAt(1, "meta", "content", "text/html; charset=utf-8", "http-equiv" , "Content-Type");
-                	el.elementAt(2, "meta", "name", "viewport", "content" , "width=device-width, initial-scale=1.0");
-                }
-              } 
-            
-        }; 
-        configuration.add("meta", rendererFilter, "before:*"); 
-        
-        if (enableAnalytics) {
-			configuration.addInstance("GAnalyticsScript", GAnalyticsScriptsInjector.class, "after:DocumentLinker");
+	}
+
+	@Contribute(MarkupRenderer.class)
+	public static void addMetaRenderFilter(
+			OrderedConfiguration<MarkupRendererFilter> configuration,
+			final Environment environment,
+			@Symbol("enableAnalytics") final boolean enableAnalytics) {
+		MarkupRendererFilter rendererFilter = new MarkupRendererFilter() {
+			public void renderMarkup(MarkupWriter writer,
+					MarkupRenderer renderer) {
+
+				renderer.renderMarkup(writer);
+				Element el = writer.getDocument().getRootElement().find("head");
+				if (el != null) {
+					el.elementAt(0, "meta", "content", "IE=edge", "http-equiv",
+							"X-UA-Compatible");
+					el.elementAt(1, "meta", "content",
+							"text/html; charset=utf-8", "http-equiv",
+							"Content-Type");
+					el.elementAt(2, "meta", "name", "viewport", "content",
+							"width=device-width, initial-scale=1.0");
+				}
+			}
+
+		};
+		configuration.add("meta", rendererFilter, "before:*");
+
+		if (enableAnalytics) {
+			configuration.addInstance("GAnalyticsScript",
+					GAnalyticsScriptsInjector.class, "after:DocumentLinker");
 		}
 
-    }
-	
-	
-    public RequestExceptionHandler buildAppRequestExceptionHandler(
-            final Logger logger,
-            final ResponseRenderer renderer,
-            final ComponentSource componentSource)
-    {
-        return new RequestExceptionHandler()
-        {
-            public void handleRequestException(Throwable exception) throws IOException
-            {
-                logger.error("Unexpected runtime exception: " + exception.getMessage(), exception);
+	}
 
-                renderer.renderPageMarkupResponse("Index");
-            }
-        };
-    }
+	public RequestExceptionHandler buildAppRequestExceptionHandler(
+			final Logger logger, final ResponseRenderer renderer,
+			final ComponentSource componentSource) {
+		return new RequestExceptionHandler() {
+			public void handleRequestException(Throwable exception)
+					throws IOException {
+				logger.error(
+						"Unexpected runtime exception: "
+								+ exception.getMessage(), exception);
 
-    public void contributeServiceOverride(
-            MappedConfiguration<Class, Object> configuration,
+				renderer.renderPageMarkupResponse("Index");
+			}
+		};
+	}
 
-            @Local
-            RequestExceptionHandler handler)
-    {
-        configuration.add(RequestExceptionHandler.class, handler);
-    }
+	public void contributeServiceOverride(
+			MappedConfiguration<Class, Object> configuration,
+
+			@Local RequestExceptionHandler handler) {
+		configuration.add(RequestExceptionHandler.class, handler);
+	}
+
+	public static void contributeIgnoredPathsFilter(
+			Configuration<String> configuration) {
+		configuration.add("/apis/.*");
+	}
 }
