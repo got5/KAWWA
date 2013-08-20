a6e69795227f449e64b99ac00ced62651408e7d7
Emmanuel <demey.emmanuel@gmail.com>
2013-08-20 10:24:38 +0200

    Delete pom.xml, and init Spock Tests

---
+++ Portal/src/test/groovy/awl/frontsolutions/SampleTest.groovy
@@ -0,0 +1,16 @@
+package awl.frontsolutions
+
+import spock.lang.Specification
+
+class SampleTest extends Specification{
+	def "length of Spock's and his friends' names"() {
+		expect:
+		name.size() == length
+
+		where:
+		name     | length
+		"Spock"  | 5
+		"Kirk"   | 4
+		"Scotty" | 6
+	}
+}
