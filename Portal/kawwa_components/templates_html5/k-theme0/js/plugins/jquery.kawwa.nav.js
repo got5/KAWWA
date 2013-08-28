2820d7b8d9209f3ecaa450a584d08d59485fee6d
Angela Ricci <angela.ricci@atos.net>
2013-08-28 17:46:54 +0200

    Update jquery.kawwa.nav.js

--- Portal/kawwa_components/templates_html5/k-theme0/js/plugins/jquery.kawwa.nav.js
+++ Portal/kawwa_components/templates_html5/k-theme0/js/plugins/jquery.kawwa.nav.js
@@ -1,3 +1,4 @@
+	
 /* Based on FlexNav.js 0.3.2
  * Copyright 2013, Jason Weaver http://jasonweaver.name
  * Released under the WTFPL license
@@ -67,6 +68,7 @@
 
 		// Toggle for nav menu
 		$this.siblings('p.control').children('a').click(function(event) {
+			event.preventDefault();
 			$this.slideToggle(settings.animationSpeed);
 		});
 
