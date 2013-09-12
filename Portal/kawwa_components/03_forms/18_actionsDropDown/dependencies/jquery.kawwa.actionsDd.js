9aaaf251f60dd11a15d1e4d739b0f9c9bfad6484
Emmanuel <demey.emmanuel@gmail.com>
2013-09-12 14:30:18 +0200

    The Random ID setted to the list was not random

--- Portal/kawwa_components/03_forms/18_actionsDropDown/dependencies/jquery.kawwa.actionsDd.js
+++ Portal/kawwa_components/03_forms/18_actionsDropDown/dependencies/jquery.kawwa.actionsDd.js
@@ -1,51 +1,51 @@
 /* KAWWA - May, 2013
-* 
+ *
  * A dropdown list of actions
-*/
+ */
 
 
 (function($) {
-                $.fn.actionsDd = function(options) {
-                               
-                               var $this = $(this);
+    $.fn.actionsDd = function(options) {
 
-                               $this.each(function() {
+        var $this = $(this);
 
-                                               var $zControl = $(this).children('button');
-                               
-                                               var zListId = 'list' + Math.floor(Math.random()*11); 
-                                               var zList = '#' + zListId;
+        $this.each(function() {
 
-                                               // ARIA
-                                               $(this).find('div.content').attr('id', zListId);
-                                               $zControl.attr('aria-owns', zListId);
-                                               $zControl.attr('aria-expanded', false);
-                                               
-                                               // Prepares list
-                                               $(zList).css('display', 'none');
-                                               
-                                               $(zList).find('a').click(function(){
-                                                                              $(zList).slideToggle('fast');
-                                                               });
-                                               // Activates control
-                                               $zControl.click(function(event){
-                                                               event.preventDefault();
-                                                               $(zList).slideToggle('fast');
-                                                               $(this).toggleClass('active');
-                                                               
-                                                               if($(this).hasClass('active')) {
-                                                                              $(this).attr('aria-expanded', true);
-                                                               } else {
-                                                                              $(this).attr('aria-expanded', false);
-                                                               }
-                                                               
-                                                               
-                                               });
-                               });
+            var $zControl = $(this).children('button');
+
+            var zListId = 'list' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+            var zList = '#' + zListId;
+
+            // ARIA
+            $(this).find('div.content').attr('id', zListId);
+            $zControl.attr('aria-owns', zListId);
+            $zControl.attr('aria-expanded', false);
+
+            // Prepares list
+            $(zList).css('display', 'none');
+
+            $(zList).find('a').click(function(){
+                $(zList).slideToggle('fast');
+            });
+            // Activates control
+            $zControl.click(function(event){
+                event.preventDefault();
+                $(zList).slideToggle('fast');
+                $(this).toggleClass('active');
+
+                if($(this).hasClass('active')) {
+                    $(this).attr('aria-expanded', true);
+                } else {
+                    $(this).attr('aria-expanded', false);
+                }
 
 
-                               
+            });
+        });
 
-                };
+
+
+
+    };
 
 })(jQuery);
