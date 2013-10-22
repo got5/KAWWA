135719755ae480eafcf4dd11da757f221af3bd62
Emmanuel <demey.emmanuel@gmail.com>
2013-10-22 11:16:52 +0200

    Issue #96 input Search Components should have type=search

--- Portal/src/test/groovy/awl/frontsolutions/DownloadPageSpec.groovy
+++ Portal/src/test/groovy/awl/frontsolutions/DownloadPageSpec.groovy
@@ -4,12 +4,38 @@ import geb.spock.GebSpec
 import spock.lang.Unroll
 
 class DownloadPageSpec extends GebSpec{
-    def "Email field should have a type=email"(){
-        when:
-        go "/Portal/Download"
+    @Unroll
+    def "nav should have role=navigation for #page"(){
+        setup:
+        go page
 
-        then:
-        $("form[id='downloadForm'] input[type='email']") .size() == 1
+        expect:
+        $("nav[role='navigation']") .size() >= 1
+
+        where:
+        page << ["/Portal/Index",
+                "/Portal/AboutKawwa",
+                "/Portal/Accessibility",
+                "/Portal/Angular",
+                "/Portal/Component",
+                "/Portal/ComponentApproach",
+                "/Portal/Contact",
+                "/Portal/Documentation",
+                "/Portal/Download",
+                "/Portal/Faq",
+                "/Portal/HowTo",
+                "/Portal/IeFix",
+                "/Portal/KawwaPortal",
+                "/Portal/License",
+                "/Portal/Releases",
+                "/Portal/Releases",
+                "/Portal/Login",
+                "/Portal/Tapestry",
+                "/Portal/Templates",
+                "/Portal/UnobtrusiveApproach",
+                "/Portal/Updates"]
+
     }
+
 }
 
