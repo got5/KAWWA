4259626bdc19378051a0808862f1ed3a8ba1d90f
Angela Ricci <angela.ricci@atos.net>
2014-01-23 11:05:34 +0100

    Chenged number of items displayed/window size

--- Portal/kawwa_components/05_blocks/04_carousel/snippet.js
+++ Portal/kawwa_components/05_blocks/04_carousel/snippet.js
@@ -2,6 +2,12 @@
 /* As many display problems may be found in IE versions 7 and 6, you may consider a graceful degradation by skipping this plugin for them */
 /* Number of items in the carousel are defined by the browser window width */
 
+// IE version sniffing - only for tests on X-UA-Compatible 
+var ie6 = (navigator.appName == "Microsoft Internet Explorer" && parseInt(navigator.appVersion) == 4 && navigator.appVersion.indexOf("MSIE 6.0") != -1);
+var ie7 = (navigator.appName == "Microsoft Internet Explorer" && parseInt(navigator.appVersion) == 4 && navigator.appVersion.indexOf("MSIE 7.0") != -1);
+var ie8 = (navigator.appName == "Microsoft Internet Explorer" && parseInt(navigator.appVersion) == 4 && navigator.appVersion.indexOf("MSIE 8.0") != -1);
+
+
 $(document).ready(function(){
 	if (!(ie6 || ie7)) {
  		if(jQuery.fn.jcarousel) {
@@ -13,12 +19,14 @@ $(document).ready(function(){
 	 		} else if ($(window).width()>=480) {
 	 			jQuery('.k-carousel').jcarousel({
 		    		size: '7',
-		    		visible: '3'
+		    		visible: '3',
+		    		scroll: 3
 		    	});
 	 		} else {
 	 			jQuery('.k-carousel').jcarousel({
-		    		size: '5',
-		    		visible: '2'
+		    		size: '7',
+		    		visible: '1',
+		    		scroll: 1
 		    	});
 	 		}
  		}
