5b2063278ee8f53e5f3a4b69f65fbf914d8ef6c7
Angela Ricci <angela.ricci@atos.net>
2014-01-23 10:56:11 +0100

    Calls the new plugin

--- Portal/kawwa_components/05_blocks/03_accordion/snippet.js
+++ Portal/kawwa_components/05_blocks/03_accordion/snippet.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-	if(jQuery.ui && jQuery.ui.accordion){
-		jQuery( ".k-accordion" ).accordion({
-			heightStyle: "content"
-		});
+	if(jQuery.fn.kAccordion) {
+		jQuery( ".k-accordion" ).kAccordion();
 	}
 });
