1be23829c5637715a29f37d751c516daa138f775
Angela Ricci <angela.ricci@atos.net>
2014-01-27 10:40:11 +0100

    Replaced call for collapsible plugin
    Calls the new plugin now

--- Portal/kawwa_components/08_ecommerce/10_inBasket/snippet.js
+++ Portal/kawwa_components/08_ecommerce/10_inBasket/snippet.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
-	if(jQuery.ui && jQuery.ui.panel) {
-		jQuery('.k-panel').panel();
+	if(jQuery.fn.collapsiblePanel) {
+		jQuery( ".k-panel" ).collapsiblePanel();
 	}
 	
+	/* For ptoduct quantity */
 	function incrementTest() {
 		var i = document.createElement('input');
 		i.setAttribute('type', 'number');
 		return i.type !== 'text';
 	}
+		
 	var incTest = incrementTest();
-
+	
 	if (jQuery.fn.uppydowner) {
 		if(!incTest) {
-			jQuery('input[type=number]').attr('class', 'uppydowner');
+			jQuery('input[type=number]').addClass('uppydowner');
 			jQuery('input.uppydowner').uppydowner();
-				
-		} else {
-			jQuery('input[type=number]').css('width', '3em');
 		}
 	}
 	
