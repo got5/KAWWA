9ddd32287906188360d0e5717b6d2caf57453f82
Angela Ricci <angela.ricci@atos.net>
2014-01-27 10:10:01 +0100

    Update snippet.js

--- Portal/kawwa_components/08_ecommerce/05_productGallery/snippet.js
+++ Portal/kawwa_components/08_ecommerce/05_productGallery/snippet.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	if(jQuery.fn.jqzoom) {
+	if(jQuery.fn.jqzoom && $(window).width()>=625) {
 		jQuery('.jqzoom').jqzoom ({
             zoomType: 'standard',
             lens: true,
             preloadImages: true,
             alwaysOn: false
         });
+	} else {
+		jQuery('.thumblist').css('display', 'none');
 	}
 });
