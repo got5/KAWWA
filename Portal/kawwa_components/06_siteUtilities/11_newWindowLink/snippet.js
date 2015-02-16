1c54c9a8f39f010518af0fe413909b20a832cc6c
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-16 11:17:55 +0100

    Review jQuery implementation

--- Portal/kawwa_components/06_siteUtilities/11_newWindowLink/snippet.js
+++ Portal/kawwa_components/06_siteUtilities/11_newWindowLink/snippet.js
@@ -2,16 +2,14 @@
 	'use strict';
 
 	/* You may pass the target name as a variable / defaults to "_blank" */
-	function openNewWindow(windowName) {
-		if ($('a.k-new-window')) {
-			$('a.k-new-window').click(function(){
-				var zeName = (windowName) ? windowName : '_blank';
-				var zeTarget = $(this).attr('href');
-				window.open(zeTarget, zeName);
-				return false;
-			});
-		}
-	}
+    function openNewWindow(windowName) {
+        $('a.k-new-window').click(function(){
+            var zeName = (windowName) ? windowName : '_blank';
+            var zeTarget = $(this).attr('href');
+            window.open(zeTarget, zeName);
+            return false;
+        });
+    }
 
 	$(document).ready(function(){
 		openNewWindow();
