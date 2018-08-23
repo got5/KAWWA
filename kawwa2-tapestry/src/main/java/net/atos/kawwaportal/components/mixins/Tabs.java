d2a8ee49eaf61fb672135709a009c20d8aa17fad
NOURREDINE KHADRI <nourredine.khadri@atos.net>
2018-08-23 16:36:02 +0200

    fix issue #171, replace @ImportJQueryUI deprecated annotation, fix script's file path

--- kawwa2-tapestry/src/main/java/net/atos/kawwaportal/components/mixins/Tabs.java
+++ kawwa2-tapestry/src/main/java/net/atos/kawwaportal/components/mixins/Tabs.java
@@ -1,6 +1,6 @@
 package net.atos.kawwaportal.components.mixins;
 
-import org.got5.tapestry5.jquery.ImportJQueryUI;
+import org.apache.tapestry5.annotations.Import;
 import org.got5.tapestry5.jquery.mixins.ui.Widget;
 
 /**
@@ -8,7 +8,7 @@ import org.got5.tapestry5.jquery.mixins.ui.Widget;
  * @tapestrydoc
  * @component_version 1.1
  */
-@ImportJQueryUI(value = {"jquery.ui.widget", "jquery.ui.tabs"})
+@Import(module = "tjq/vendor/ui/tabs")
 public class Tabs extends Widget{
 
 }
